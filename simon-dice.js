db41f3720f338f36fb0212d4638f42db9581623a
Federico Aldrighetti <56283793+faldrighetti@users.noreply.github.com>
2022-03-17 17:01:28 -0300

    Add files via upload

---
+++ simon-dice.js
@@ -0,0 +1,41 @@
+let secuenciaIA = []
+let secuenciaUsuario = []
+let puntos = 0;
+
+function mostrarPuntos(){
+
+}
+
+function continuarPartida(){
+
+}
+
+function fallar(){
+
+
+    mostrarBotonReiniciar();
+}
+
+function mostrarBotonReiniciar(){
+
+}
+
+
+/*
+
+Armar 4 rectángulos de colores
+Asignarlos como botones del juego
+Botón de empezar, para elegir el cuadrado random 
+Declarar dos arrays: 
+-uno vacío de lo que hace el jugador (1)
+-el otro vacío, pero que al empezar se le elige un cuadrado random (con un timer se le cambia el color a 
+    uno más claro por un segundo). Si el clic es correcto, se elige otro random y así. (2)
+Al hacerle clic al correcto, se hace un push al array 1
+Si se le da el clic al equivocado, sale un mensaje "Perdiste!". No hay push ni nada porque muere el juego ahí. 
+Se pone otro mensaje de "Hiciste X puntos" (contador += 1) y botón para reiniciar.
+
+
+2- Hacer formularios anteriores en CSS con bootstrap, más paquetes json
+
+3- Volver a ver clase 8
+*/
