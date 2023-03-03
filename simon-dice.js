cbfa8a3b882e6972f2f31454f4e5b0d064c70c92
Federico Aldrighetti <f.a.aldrighetti@gmail.com>
2023-03-03 18:26:40 -0300

    Elimina funciones no utilizadas

--- simon-dice.js
+++ simon-dice.js
@@ -111,33 +111,7 @@ function habilitarUsuario(){
     });
 }
 
-function mostrarPuntos(){
-    return puntos*10;
-}
-
-function continuarPartida(){
-    if(compararArrays(secuenciaIA, secuenciaUsuario)){
-        encenderColor();
-    }
-    secuenciaUsuario = [];
-}
-
 function perderPartida(){
     deshabilitarUsuario();
     actualizarEstado('Perdiste, tocá "Iniciar partida" para volver a jugar');
 }
-
-/*
-SI Armar 4 rectángulos de colores
-SI Asignarlos como botones del juego
-SI Botón de empezar, para elegir el cuadrado random 
-Declarar dos arrays: 
--uno vacío de lo que hace el jugador (1)
--el otro vacío, pero que al empezar se le elige un cuadrado random (con un timer se le cambia el color a 
-    uno más claro por un segundo). Si el clic es correcto, se elige otro random y así. (2)
-Al hacerle clic al correcto, se hace un push al array 1
-Si se le da el clic al equivocado, sale un mensaje "Perdiste!". No hay push ni nada porque muere el juego ahí. 
-Se pone otro mensaje de "Hiciste X puntos" (contador += 1) y botón para reiniciar.
-
-2- Hacer formularios anteriores en CSS con bootstrap, más paquetes json
-*/
